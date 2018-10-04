a962f40acd1cba2d0cac31abf594d1b94444d915
Maksim Manylow <maksimmanylow@gmail.com>
2018-10-05 02:37:58 +0300

    added auth guard tags for navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,53 +1,40 @@
 <!-- Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light fixed-top <?= $isIndex ? '' : 'navbar-shrink' ?>" id="mainNav">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top {{ Request::is('/site/index') || Request::is('/site/index') ? '' : 'navbar-shrink'}}" id="mainNav">
   <div class="container">
     <a class="navbar-brand js-scroll-trigger" href="/">ВОЕНМЕХ.автошкола</a>
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+      aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
-          <a class="nav-link <?= (Yii::$app->request->url == " /site " || Yii::$app->request->url == "/ ") ? 'active' : '' ?>" href="/">Главная</a>
+          <a class="nav-link {{ App\Http\Helpers::set_active(['/', '/site', '/site/index']) }}" href="/">Главная</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  <?= (Yii::$app->request->url == " /site/groups ") ? 'active' : '' ?>" href="/site/groups">Запись</a>
+          <a class="nav-link  {{ App\Http\Helpers::set_active(['/site/groups']) }}" href="/site/groups">Запись</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link  <?= (Yii::$app->request->url == " /site/news ") ? 'active' : '' ?>" href="/site/news">Новости</a>
+          <a class="nav-link  {{ App\Http\Helpers::set_active(['/site/news']) }}" href="/site/news">Новости</a>
         </li>
 
         <li class="nav-item">
           <a class="nav-link" target="_blank" href="https://vk.com/voenmeh.avtoshkola">VK</a>
         </li>
 
-        <li class="nav-item">
-          <a class="nav-link" target="_blank" href="https://www.facebook.com/bstuattcru/">Facebook</a>
-        </li>
-
 
 
         <li class="nav-item">
 
-          <?php if (Yii::$app->user->isGuest) {
-?>
-            <a class="nav-link" href="/site/login">Вход</a>
-            <?php
-} else {
-?>
-
-              <?php if (Yii::$app->user->can('admin')) {
-?>
-                <a class="nav-link" href="/admin">Панель управления</a>
-                <?php
-} else {
-?>
-                  <a class="nav-link" href="/user">Личный кабинет</a>
-                  <?php
-} ?>
-
-                    <?php
-} ?>
+          @auth('admin')
+          <a class="nav-link" href="/admin">Панель управления</a> 
+          @endauth 
+          @auth('user')
+          <a class="nav-link" href="/user">Личный кабинет</a> 
+          @endauth 
+          @guest('admin')
+          <a class="nav-link" href="/site/login">Вход</a> 
+          @endguest
 
         </li>
       </ul>
