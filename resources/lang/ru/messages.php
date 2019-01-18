dcc298c7c5debd4ed706f425d5818dda95263e05
Maksim Manylow <maksimmanylow@gmail.com>
2019-01-18 12:41:23 +0300

    changed default fallback locale to ru and added translations for the signup notification template

---
+++ resources/lang/ru/messages.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Hello!' => 'Здравствуйте!',
+    'Whoops!' => 'Ой...',
+    'Regards' => 'С уважением',
+    "If you’re having trouble clicking the \":actionText\" button, copy and paste the URL below\n" => "Если не работает кнопка \":actionText\", скопируйте и перейдите по ссылке ниже\n".
+    'в браузере: [:actionURL](:actionURL)',
+];
