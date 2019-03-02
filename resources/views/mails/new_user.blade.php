75a491329d9eb1259dc6dea86990457d78358e21
Maksim Manylow <maksimmanylow@gmail.com>
2019-03-02 18:01:21 +0300

    FIX переменные в email шаблоне

--- resources/views/mails/new_user.blade.php
+++ resources/views/mails/new_user.blade.php
@@ -1,9 +1,9 @@
 <p><u>Данные пользователя:</u></p>
 <div>
-<p><b>Фамилия:</b>&nbsp;{{ $data['name'] }}</p>
-<p><b>Имя:</b>&nbsp;{{ $data['surname'] }}</p>
-<p><b>Email:</b>&nbsp;{{ $data['email'] }}</p>
-<p><b>Телефон:</b>&nbsp;{{ $data['phone'] }}</p>
-<p><b>Группа:</b>&nbsp;{{ $data['group'] }}</p>
+<p><b>Фамилия:</b>&nbsp;{{ $name }}</p>
+<p><b>Имя:</b>&nbsp;{{ $surname }}</p>
+<p><b>Email:</b>&nbsp;{{ $email }}</p>
+<p><b>Телефон:</b>&nbsp;{{ $phone }}</p>
+<p><b>Группа:</b>&nbsp;{{ $group }}</p>
 </div>
  
